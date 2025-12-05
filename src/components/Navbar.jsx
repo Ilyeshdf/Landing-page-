2bdeec448287a2c42b6cbe14814896a2f9817dae
chaker-zeghar <zeghdarchaker@gmail.com>
2025-12-06 00:45:16 +0100

    fix logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
             <div className="container mx-auto px-4 md:px-6 flex justify-between items-center">
                 {/* Logo */}
                 <div className="flex flex-row leading-none items-center">
-                    <img src="src\assets\ChatGPT_Image_Dec_5__2025__01_03_42_AM-removebg-preview.png" alt="Zer3aX Logo" className="h-12 w-12" />
+                    <img src="https://res.cloudinary.com/dfxhtf6xh/image/upload/v1764978209/ChatGPT_Image_Dec_5__2025__01_03_42_AM-removebg-preview_u4rqio.png" alt="Zer3aX Logo" className="h-12 w-12" />
                     <span className="text-2xl font-bold text-dark-green font-sans tracking-tight">Zer3aX</span>
                 </div>
 
